567e7b29a43b4373d1c9e5dd2c7b4ea5aa56d033
Renan <49691608+renanponick@users.noreply.github.com>
2024-05-29 14:36:33 -0300

    Update route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 const controllers = new ControllerExercicio()
 
-router.post("/somar", controllers.Somar);
+router.post("/api/somar", controllers.Somar);
 router.post("/api/subtrair", controllers.Subtrair);
 router.post("/api/dividir", controllers.Multiplicar);
 router.post("/api/multiplicar", controllers.Dividir);
