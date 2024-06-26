d407a6dbfc8320d62b29a243c1b12739eeb8071b
rkschitz <ruhankaio900@gmail.com>
2024-06-26 00:10:28 -0300

    commit

--- test/unit/somar.test.js
+++ test/unit/somar.test.js
@@ -1,28 +1,28 @@
 const Service = require('../../src/services/service');
 
-describe('Teste de multiplicação', () => {
+describe('Teste de soma', () => {
     let service;
 
     beforeAll(() => {
         service = new Service();
     });
 
-    test('Deve multiplicar dois números', () => {
-        const result = service.Multiplicar(2, 2);
+    test('Deve somar dois números', () => {
+        const result = service.Somar(2, 2);
         expect(result).toBe(4);
     });
 
     test('Deve retornar erro se num1 não for informado', () => {
-        const result = () => service.Multiplicar(null, 13);
+        const result = () => service.Somar(null, 13);
         expect(result).toThrowError('Todos os valores devem ser informados');
     });
 
     test('Deve retornar erro se num2 não for informado', () => {
-        const result = () => service.Multiplicar(12, null);
+        const result = () => service.Somar(12, null);
         expect(result).toThrowError('Todos os valores devem ser informados');
     });
 
     test('Deve retornar erro se num1 não for um número', () => {
-        expect(() => service.Multiplicar('doze', 13)).toThrowError('Todos os valores devem ser numéricos');
+        expect(() => service.Somar('doze', 13)).toThrowError('Todos os valores devem ser numéricos');
     });
 })
